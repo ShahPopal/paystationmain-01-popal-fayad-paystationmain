3207489e2dfd82ae2f9f3023d75b1f06f5431bec
Shah <tuo09456@temple.edu>
2022-01-25 00:19:38 -0500

    Added import for HashMap Made a private integer for a new HashMap, where map is an integer type and created a map called inserted which can take integer values. Made a private integer of count_5, count_10, and count_25 to equal 0 for cases.

--- src/main/java/edu/temple/cis/paystation/PayStationImpl.java
+++ src/main/java/edu/temple/cis/paystation/PayStationImpl.java
@@ -19,11 +19,14 @@
  */
 
 package edu.temple.cis.paystation;
+import java.util.HashMap;
 
 public class PayStationImpl implements PayStation {
     
     private int insertedSoFar;
     private int timeBought;
+    private static Map<Integer, Integer>insertedCoins = new HashMap<>();
+    private int count_5, count_10, count_25 = 0;
 
     @Override
     public void addPayment(int coinValue)
@@ -41,6 +44,8 @@ public class PayStationImpl implements PayStation {
 
     @Override
     public int readDisplay() {
+
+        System.out.println("You have " + timeBought + " minutes with " + insertedSoFar + " cents put in.");
         return timeBought;
     }
 
